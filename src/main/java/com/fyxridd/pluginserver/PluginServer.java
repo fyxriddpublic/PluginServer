1681ca06639b6f5766650437ec4d2792bb7d3ac2
fyxridd <fyxridd@163.com>
2016-05-06 19:26:13 +0800

    更新

--- src/main/java/com/fyxridd/pluginserver/PluginServer.java
+++ src/main/java/com/fyxridd/pluginserver/PluginServer.java
@@ -1,20 +1,18 @@
 package com.fyxridd.pluginserver;
 
-import com.fyxridd.netty.common.Listener;
-import com.fyxridd.netty.common.MessageMain;
-import com.fyxridd.netty.common.coder.Lv1Decoder;
-import com.fyxridd.netty.common.coder.Lv1Encoder;
-import com.fyxridd.netty.common.coder.Lv2Decoder;
-import com.fyxridd.netty.common.coder.Lv2Encoder;
+import com.fyxridd.netty.common.coder.ByteBufToPacketContextDecoder;
+import com.fyxridd.netty.common.coder.PacketContextToByteBufEncoder;
 import com.fyxridd.netty.common.debug.MessageDebugDecoder;
 import com.fyxridd.netty.common.debug.MessageDebugEncoder;
+import com.fyxridd.netty.common.packet.PacketContext;
 import com.fyxridd.netty.common.util.Util;
-import com.fyxridd.pluginserver.messages.Login;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.compression.JdkZlibDecoder;
+import io.netty.handler.codec.compression.JdkZlibEncoder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -40,29 +38,26 @@ public class PluginServer implements InitializingBean {
                         public void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline()
                                     //In
-                                    .addLast(new MessageDebugDecoder(), new Lv1Decoder(), new Lv2Decoder())
+                                    .addLast(new MessageDebugDecoder(), new JdkZlibDecoder(), new ByteBufToPacketContextDecoder(), new SimpleChannelInboundHandler<PacketContext>() {
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, PacketContext msg) throws Exception {
+
+                                        }
+                                    })
                                     //Out
-                                    .addLast(new Lv2Encoder(), new Lv1Encoder(), new MessageDebugEncoder());
+                                    .addLast(new MessageDebugEncoder(), new JdkZlibEncoder(), new PacketContextToByteBufEncoder());
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            b.bind(config.getPort()).addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture future) throws Exception {
-                    future.channel().closeFuture();
-                }
-            });
-
-            //注册监听
-            MessageMain.getMessageManager().registerNamespace(NAME_SPACE);
-            MessageMain.getMessageManager().listen(NAME_SPACE, Login.class, new Listener<Login>() {
-                @Override
-                public void onEvent(Login msg) {
-
-                }
-            });
+            b.bind(config.getPort()).sync().channel().closeFuture().sync();
+//                    .addListener(new ChannelFutureListener() {
+//                @Override
+//                public void operationComplete(ChannelFuture future) throws Exception {
+//                    future.channel().closeFuture();
+//                }
+//            });
         } finally {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
